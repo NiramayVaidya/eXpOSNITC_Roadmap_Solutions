1513ef8ee5f06d2d5f01b0b22c60fe3a69a485ad
Niramay_Vaidya <niramay.vaidya@gmail.com>
2020-08-22 18:50:10 +0530

    Added solutions for the intermediate step 15 of the roadmap

---
+++ xfs-interface/batch_files/resource_manager_terminal.bat
@@ -0,0 +1,6 @@
+load --init ../expl/expl_progs/oddnos_1to99_init.xsm
+load --exec ../expl/expl_progs/pid_2.xsm
+load --int=7 ../spl/spl_progs/int7_resource_manage_terminal.xsm
+load --module 7 ../spl/spl_progs/boot_module_resource_manage_terminal.xsm
+load --module 4 ../spl/spl_progs/module_4_terminal_write.xsm
+load --module 0 ../spl/spl_progs/module_0_terminal_handling.xsm
