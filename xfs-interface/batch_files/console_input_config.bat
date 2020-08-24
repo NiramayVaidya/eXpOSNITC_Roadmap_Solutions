07f3e8217fd6b146c350266394d811fe0404246f
Niramay_Vaidya <niramay.vaidya@gmail.com>
2020-08-24 13:02:46 +0530

    Added solutions for the intermediate step 16 of the roadmap

---
+++ xfs-interface/batch_files/console_input_config.bat
@@ -0,0 +1,5 @@
+load --init ../expl/expl_progs/gcd_euclidean.xsm
+load --module 7 ../spl/spl_progs/boot_module_console_input.xsm
+load --module 4 ../spl/spl_progs/module_4_terminal_read_write.xsm
+load --int=console ../spl/spl_progs/console_interrupt_handler.xsm
+load --int=6 ../spl/spl_progs/int6_read.xsm
